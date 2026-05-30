874787d37f4e41d8054cf03b34340683b392e57e
Fernando Yael Herrera Tamayo <00038623@uca.edu.sv>
2026-05-30 00:25:22 -0600

    coemnzando proyecto y agregando dependencias

---
+++ Backend_TallerAutomotriz/src/main/java/com/example/backend_tallerautomotriz/BackendTallerAutomotrizApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_tallerautomotriz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTallerAutomotrizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendTallerAutomotrizApplication.class, args);
+    }
+
+}
---
+++ Backend_TallerAutomotriz/src/test/java/com/example/backend_tallerautomotriz/BackendTallerAutomotrizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_tallerautomotriz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTallerAutomotrizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
